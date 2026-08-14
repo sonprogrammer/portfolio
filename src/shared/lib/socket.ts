bd3b8294f8e05ac515f5e20f467aa9667591cc7b
sonprogrammer <ods04138@gmail.com>
2026-08-15 03:01:10 +0900

    socket url

--- src/shared/lib/socket.ts
+++ src/shared/lib/socket.ts
@@ -5,7 +5,7 @@ let socket: Socket | null = null
 
 export function getSocket() {
     if(!socket){
-        socket = io({
+        socket = io(process.env.NEXT_PUBLIC_SOCKET_URL,{
             autoConnect: false
         })
     }
