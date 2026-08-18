634e72679b7e32fdf2a6d2a119372142101fbd14
sonprogrammer <ods04138@gmail.com>
2026-08-18 22:18:16 +0900

    x

--- src/app/api/visitor/route.ts
+++ src/app/api/visitor/route.ts
@@ -1,56 +1,73 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Client } from '@notionhq/client'
 
-const notion = new Client({auth: process.env.NOTION_TOKEN})
+const notion = new Client({ auth: process.env.NOTION_TOKEN })
 
 const DATABASE_ID = process.env.NOTION_DATA_SOURCE_ID
 
-export async function POST(req: NextRequest){
+export async function POST(req: NextRequest) {
     try {
-        if(!DATABASE_ID){
-            return NextResponse.json({message: 'there is no notions database id '},{status: 500})
+
+        if (process.env.NODE_ENV !== 'production') {
+            return NextResponse.json({ success: true })
+        }
+        if (!DATABASE_ID) {
+            return NextResponse.json({ message: 'there is no notions database id ' }, { status: 500 })
         }
 
         const body = await req.json()
 
-        const { page, referrer, device} = body
+        const { page, referrer, device } = body
 
         await notion.pages.create({
-            parent: {database_id: DATABASE_ID},
+            parent: { database_id: DATABASE_ID },
             properties: {
-                '유입 경로' : {
+                이름: {
                     title: [
                         {
                             text: {
-                                content: referrer ||'직접 방문'
-                            }
-                        }
-                    ]
+                                content: '포트폴리오 방문',
+                            },
+                        },
+                    ],
                 },
-                '페이지' : {
+
+                접속시간: {
+                    date: {
+                        start: new Date().toISOString(),
+                    },
+                },
+
+                경로: {
                     rich_text: [
                         {
                             text: {
-                                content: page || '/'
-                            }
-                        }
-                    ]
+                                content: page || '/',
+                            },
+                        },
+                    ],
                 },
-                '방문 시간': {
-                    date: {
-                        start: new Date().toISOString()
-                    }
+
+                유입경로: {
+                    rich_text: [
+                        {
+                            text: {
+                                content: referrer || '직접 방문',
+                            },
+                        },
+                    ],
                 },
-                '디바이스': {
+
+                기기: {
                     select: {
-                        name: device || 'unknown'
-                    }
-                }
-            }
-        })
-        return NextResponse.json({success: true})
+                        name: device || 'desktop',
+                    },
+                },
+            },
+        });
+        return NextResponse.json({ success: true })
     } catch (error) {
         console.log('방문 기록 저장 실패', error)
-        return NextResponse.json({message: '방문 기록 저장  실패'}, {status: 500})
+        return NextResponse.json({ message: '방문 기록 저장  실패' }, { status: 500 })
     }
 }
